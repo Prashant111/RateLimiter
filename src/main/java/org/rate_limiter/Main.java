824b39543c6c407768047a8a0338035425f09af0
Prashant Singh3 <prashant_singh3@epam.com>
2023-10-02 07:01:43 +0530

    Initial commit - rate limiter

---
+++ src/main/java/org/rate_limiter/Main.java
@@ -0,0 +1,7 @@
+package org.rate_limiter;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/rate_limiter/algo/RateLimiter.java
@@ -0,0 +1,4 @@
+package org.rate_limiter.algo;
+
+public interface RateLimiter {
+}

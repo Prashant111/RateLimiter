ac5ad769941ee24ba27e7469c9fba6b5dc55577c
Prashant Singh <prince.nsit111@gmail.com>
2023-10-03 17:11:41 +0530

    Removing unnecessary code from User class

--- src/main/java/org/rate_limiter/User.java
+++ src/main/java/org/rate_limiter/User.java
@@ -6,41 +6,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static org.rate_limiter.configurations.CommonConfiguration.DEFAULT_TOKENS_PER_CREDIT;
-import static org.rate_limiter.configurations.CommonConfiguration.EXTRA_CREDITS_VALIDITY_DURATION;
-
 public class User {
 
     private final long id;
     private final SubscriptionType type;
 
     private final List<PaidCredit> paidCredits;
-    private final AtomicLong paidAdditionalRequests;
 
     public User(long id, SubscriptionType type) {
         this.id = id;
         this.type = type;
         this.paidCredits = Collections.synchronizedList(new ArrayList<>());
-        this.paidAdditionalRequests = new AtomicLong(0L);
     }
 
     public void addCredits(long extraCredits) {
         this.paidCredits.add(new PaidCredit(new AtomicLong(extraCredits), LocalDateTime.now()));
-        updatePaidAdditionRequests();
-    }
-
-    private void updatePaidAdditionRequests() {
-        long validCredits = this.paidCredits.stream()
-                                            .filter(paidCredit -> paidCredit.allocationTime()
-                                                                            .plus(EXTRA_CREDITS_VALIDITY_DURATION)
-                                                                            .isAfter(LocalDateTime.now()))
-                                            .map(PaidCredit::credits)
-                                            .map(AtomicLong::get)
-                                            .mapToLong(Long::longValue)
-                                            .sum();
-        long validRequestCount = (long) (validCredits * DEFAULT_TOKENS_PER_CREDIT);
-        System.out.println("validRequestCount = " + validRequestCount);
-        this.paidAdditionalRequests.set(validRequestCount);
     }
 
     public SubscriptionType getType() {
@@ -49,9 +29,5 @@ public class User {
 
     public List<PaidCredit> getPaidCredits() {
         return paidCredits;
-    }
-
-    public AtomicLong getPaidAdditionalRequests() {
-        return paidAdditionalRequests;
     }
 }
